5ee35f7ffa3ad32e3bb64878f4d3c56cd4733226
pulkit250399 <pulkit250399@gmail.com>
2020-05-24 16:23:05 +0530

    Second commit

---
+++ Mickeymouse.py
@@ -0,0 +1,3 @@
+Hello Everyone Myself Pulkit jain from Kota rajasthan.
+The character that i resemble the most is mickey mouse
+because of his lovable and friendly nature.He too just like me is always there for his friends, and also he is always optimistic irrespective of the situation and everyone can rely on him. Just like i can be relied upon for contributing to the good of company.
